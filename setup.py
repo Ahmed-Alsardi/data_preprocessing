4b8f6ab60e69fd8a1529073b2c953f7f18cb8af4
Ahmed AlSardi <A7Dpy@outlook.com>
2022-12-25 15:57:54 +0300

    create package locally

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='audio_data_preprocessing',
+    version='0.0.01',
+    packages=['audio_preprocessing'],
+    url='',
+    license='',
+    author='Ahmed Alsardi',
+    author_email='',
+    description=''
+)
