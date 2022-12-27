4e588539971cf7fd7ecaa2a1947a79df84b6229e
Ahmed AlSardi <A7Dpy@outlook.com>
2022-12-27 13:35:24 +0300

    refactoring

--- src/audio_preprocessing/S3DataProvider/S3SubtitleProvider.py
+++ src/audio_preprocessing/S3DataProvider/S3SubtitleProvider.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 import os
 from typing import Generator
+from dataclasses import dataclass
 
 load_dotenv()
 
@@ -10,25 +11,30 @@ ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
 SECRET_KEY = os.environ['AWS_SECRET_KEY']
 SUBTITLE_PREFIX = 'masc/clean_train/subtitles'
 SUBTITLE_DOWNLOAD_PATH = Path.cwd() / 'subtitles'
+BUCKET_NAME = "arabic-speech-data"
+
+
+@dataclass(kw_only=True)
+class S3Config:
+    access_key: str
+    secret_key: str
+    subtitle_prefix: str
+    bucket_name: str
 
 
 class S3SubtitleProvider:
-    def __init__(self, access_key: str, secret_key: str, subtitle_prefix: str,
-                 subtitle_download_path: Path, bucket_name: str = 'arabic-speech-data',
-                 page_size: int = 5):
-        self._subtitle_prefix = subtitle_prefix
+    def __init__(self, s3_config: S3Config, subtitle_download_path: Path):
+        self._s3_config = s3_config
         self._subtitle_download_path = subtitle_download_path
-        self._bucket_name = bucket_name
-        self._page_size = page_size
         # Create a session
         print('Accessing S3...')
         self._session: boto3.Session = boto3.Session(
-            aws_access_key_id=access_key,
-            aws_secret_access_key=secret_key,
+            aws_access_key_id=self._s3_config.access_key,
+            aws_secret_access_key=self._s3_config.secret_key,
         )
         # Create a resource
         self._s3 = self._session.resource('s3')
-        self._bucket = self._s3.Bucket(self._bucket_name)
+        self._bucket = self._s3.Bucket(self._s3_config.bucket_name)
         # Get the list of subtitles
         self._subtitle_list: list[str] = self._get_subtitle_list()
         print(f"Total number of subtitles: {len(self._subtitle_list)}")
@@ -47,11 +53,11 @@ class S3SubtitleProvider:
             self._bucket.download_file(subtitle, self._subtitle_download_path / subtitle.split('/')[-1])
             yield self._subtitle_download_path / subtitle.split('/')[-1]
 
-    def _get_subtitle_list(self, page_size=100) -> list[str]:
+    def _get_subtitle_list(self) -> list[str]:
         subtitle_list: list[str] = []
         print("Retrieving subtitles...")
         for page in self._bucket.objects.filter(
-                Prefix=self._subtitle_prefix).page_size(page_size).pages():
+                Prefix=self._s3_config.subtitle_prefix).pages():
             [subtitle_list.append(o.key) for o in page]
             print(f"Retrieved {len(subtitle_list)} subtitles")
         return subtitle_list
@@ -59,7 +65,13 @@ class S3SubtitleProvider:
 
 if __name__ == '__main__':
     print('initiating S3SubtitleProvider...')
-    s3 = S3SubtitleProvider(ACCESS_KEY, SECRET_KEY, SUBTITLE_PREFIX, SUBTITLE_DOWNLOAD_PATH)
+    s3_config = S3Config(
+        access_key=ACCESS_KEY,
+        secret_key=SECRET_KEY,
+        subtitle_prefix=SUBTITLE_PREFIX,
+        bucket_name=BUCKET_NAME
+    )
+    s3 = S3SubtitleProvider(s3_config=s3_config, subtitle_download_path=SUBTITLE_DOWNLOAD_PATH)
     print(f"First 3 subtitles: {s3.subtitle_list[:3]}")
     print('Downloading subtitles...')
     for i, subtitle_path in enumerate(s3.download_subtitles()):
