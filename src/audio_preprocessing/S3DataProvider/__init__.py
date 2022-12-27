43aa3879072bb71fed135531bbc5ab9de567b5c6
Ahmed AlSardi <A7Dpy@outlook.com>
2022-12-27 13:42:29 +0300

    refactoring

--- src/audio_preprocessing/S3DataProvider/__init__.py
+++
@@ -1,1 +0,0 @@
-from .S3SubtitleProvider import S3SubtitleProvider
--- src/audio_preprocessing/services/splitting_vtt.py
+++ src/audio_preprocessing/services/splitting_vtt.py
@@ -51,7 +51,7 @@ def splitter_generator(
 
 
 if __name__ == '__main__':
-    # file_path = Path.cwd().parent / "S3DataProvider" / "subtitles" / "-2vjWSEQF-Q.ar.vtt"
+    # file_path = Path.cwd().parent / "s3_data_provider" / "subtitles" / "-2vjWSEQF-Q.ar.vtt"
     file_path = Path.cwd().parent.parent.parent / "tests" / "services" / "test_data" / "test.ar.vtt"
     print(file_path.exists())
     if file_path.exists():
