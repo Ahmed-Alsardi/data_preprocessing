87af15e2e601c6613b5c6c6cd133469941b472d2
Ahmed Alsardi <ahmed.alsardi.21@gmail.com>
2023-01-12 15:52:41 +0000

    refactor code

--- src/audio_preprocessing/common_voice/audio_converter.py
+++ src/audio_preprocessing/common_voice/audio_converter.py
@@ -1,15 +1,19 @@
 from multiprocessing.pool import ThreadPool
 from pathlib import Path
+import logging
 import pandas as pd
 import tqdm
 from pydub import AudioSegment
+
+logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
 
 
 ROOT_PATH = Path("data/v11/")
 AUIDO_PATH = ROOT_PATH / "clips"
 TRAIN_TSV_PATH = ROOT_PATH / "train.tsv"
-VALID_TSV_PATH = ROOT_PATH / "dev.tsv"
+DEV_TSV_PATH = ROOT_PATH / "dev.tsv"
 TEST_TSV_PATH = ROOT_PATH / "test.tsv"
+VALIDATED_TSV_PATH = ROOT_PATH / "validated.tsv"
 TARGET_PATH = ROOT_PATH / "clips_wav"
 
 
@@ -19,22 +23,42 @@ def convert_audio(audio_path: Path, target_path: Path = TARGET_PATH):
     audio.export(target_path / audio_name, format="wav")
 
 
-def convert_dataframe_audio(df: pd.DataFrame):
+def change_df_audio_path(df: pd.DataFrame):
+    df["path"] = df["path"].apply(lambda x: x.replace(".mp3", ".wav"))
+    return df
+
+
+def convert_dataframe_audio(dataframe_path: Path):
+    df = pd.read_csv(dataframe_path, sep="\t")
+    # check if .wav in df path
+    if ".wav" in df["path"].iloc[0]:
+        logging.info("Already converted to .wav")
+        return
     audio_paths = [AUIDO_PATH / audio_name for audio_name in df["path"]]
+    logging.info(
+        "Converting %s to .wav, detecting %d files", dataframe_path, len(audio_paths)
+    )
     with ThreadPool(8) as pool:
         for _ in tqdm.tqdm(
             pool.imap_unordered(convert_audio, audio_paths), total=len(audio_paths)
         ):
             pass
+    change_df_audio_path(df).to_csv(dataframe_path, sep="\t", index=False)
+    logging.info("Finished converting %s to .wav", dataframe_path)
+
+
+def create_target_dir_if_not_exist():
+    if not TARGET_PATH.exists():
+        TARGET_PATH.mkdir(parents=True, exist_ok=True)
 
 
 def main():
-    # train_df = pd.read_csv(TRAIN_TSV_PATH, sep="\t")
-    # dev_df = pd.read_csv(VALID_TSV_PATH, sep="\t")
-    test_df = pd.read_csv(TEST_TSV_PATH, sep="\t")
-    print("=========== Converting test data")
-    convert_dataframe_audio(test_df)
-    print("=========== Finished converting test data")
+    logging.info("ROOT_PATH: %s", ROOT_PATH)
+
+    create_target_dir_if_not_exist()
+    df_paths = [TRAIN_TSV_PATH, DEV_TSV_PATH, TEST_TSV_PATH, VALIDATED_TSV_PATH]
+    for df_path in df_paths:
+        convert_dataframe_audio(df_path)
 
 
 if __name__ == "__main__":
