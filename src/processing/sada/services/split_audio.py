b18825c360ede96bb68396a836f4472ae8590d8e
Ahmed Alsardi <ahmed.alsardi.21@gmail.com>
2023-01-28 15:00:12 +0000

    try excecpt for combine audios

--- src/processing/sada/services/split_audio.py
+++ src/processing/sada/services/split_audio.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from dataclasses import dataclass
+from pprint import pprint
 from typing import Generator, Tuple, Optional
 import secrets
 import pandas as pd
@@ -13,6 +14,7 @@ class AudioSegment:
     end: float
     text: str
 
+
 @dataclass
 class AudioFile:
     filename: str
@@ -21,8 +23,12 @@ class AudioFile:
 
 
 ROOT_PATH = Path("~/dev/sada/").expanduser()
+OUTPUT_DIR = Path("../data")
 
-def combine_df_rows(df: pd.DataFrame, min_length=20) -> Generator[AudioFile, None, None]:
+
+def combine_df_rows(
+    df: pd.DataFrame, min_length=30
+) -> Generator[AudioFile, None, None]:
     """
     Combine rows of a dataframe to increase the length of the audio file
     Args:
@@ -47,19 +53,40 @@ def combine_df_rows(df: pd.DataFrame, min_length=20) -> Generator[AudioFile, Non
             segments = []
 
 
-def combine_audio_file(audio_path: Path, audio_meta: AudioFile, output_dir: Path, silence_duration: float=30) -> None:
+def combine_audio_file(
+    audio_path: Path,
+    audio_meta: AudioFile,
+    output_dir: Path,
+    silence_duration: float = 30,
+) -> True:
     """
     Combine several audio files into one audio file.
     Args:
         audio_path: Path to the audio files
         audio: AudioFile object
         output_dir: Path to the output directory
+        silence_duration: duration of the silence between the audio files
+    Returns:
+        True if the audio file is successfully created, False otherwise.
     """
-    audio = PydubAudioSegment.empty()
-    for segment in audio_meta.segments:
-        audio += PydubAudioSegment.from_wav(audio_path / segment.filename)[segment.start * 1000:segment.end * 1000]
-        audio += PydubAudioSegment.silent(duration=silence_duration)
-    audio.export(output_dir / audio_meta.filename, format="wav")
+    try:
+        audio = PydubAudioSegment.empty()
+        for segment in audio_meta.segments:
+            audio += PydubAudioSegment.from_wav(audio_path / segment.filename)[
+                int(segment.start * 1000) : int(segment.end * 1000)
+            ]
+            audio += PydubAudioSegment.silent(duration=silence_duration)
+        audio.export(output_dir / audio_meta.filename, format="wav")
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
 
 if __name__ == "__main__":
-    print(ROOT_PATH.exists())
+    df = pd.read_csv(ROOT_PATH / "train.csv")
+    for audio_meta in combine_df_rows(df):
+        pprint(audio_meta)
+        combine_audio_file(ROOT_PATH, audio_meta, OUTPUT_DIR)
+        print("Done")
+        break
