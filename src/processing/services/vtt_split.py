cf56746e5779197fb52863710b654472e55020b2
Ahmed Alsardi <ahmed.alsardi.21@gmail.com>
2023-02-13 19:50:14 +0000

    vtt split

--- src/processing/services/vtt_split.py
+++ src/processing/services/vtt_split.py
@@ -1,5 +1,7 @@
-from pathlib import Path 
+from pathlib import Path
+import webvtt
 from processing.config import Audio, AudioSegment, SourceEnum
+
 
 def vtt_split(
     vtt_path: Path,
@@ -8,4 +10,71 @@ def vtt_split(
     threshold: float = 2.0,
     source: SourceEnum = SourceEnum.MASC,
 ) -> Audio:
-    pass
+    vtt = webvtt.read(vtt_path)
+    filename = vtt_path.stem
+    segments = []
+    start = vtt[0].start_in_seconds
+    end = vtt[0].end_in_seconds
+    duration = end - start
+    text = vtt[0].text
+    for caption in vtt[1:]:
+        caption_duration = caption.end_in_seconds - caption.start_in_seconds
+        if caption.start_in_seconds - end > threshold:
+            # difference between current caption and previous caption is greater than threshold
+            if duration >= min_duration:
+                segments.append(
+                    AudioSegment(
+                        start=start,
+                        end=end,
+                        text=text,
+                        source=source,
+                        filename=f"{filename}_{len(segments)}",
+                    )
+                )
+            start, end, duration, text = (
+                caption.start_in_seconds,
+                caption.end_in_seconds,
+                caption_duration,
+                caption.text,
+            )
+        if duration + caption_duration <= max_duration:
+            # current caption can be added to the segment
+            end, duration, text = (
+                caption.end_in_seconds,
+                duration + caption_duration,
+                f"{text} {caption.text}",
+            )
+        else:
+            # current caption cannot be added to the segment
+            segments.append(
+                AudioSegment(
+                    start=start,
+                    end=end,
+                    text=text,
+                    source=source,
+                    filename=f"{filename}_{len(segments)}",
+                )
+            )
+            start, end, duration, text = (
+                caption.start_in_seconds,
+                caption.end_in_seconds,
+                caption_duration,
+                caption.text,
+            )
+    if duration >= min_duration:
+        segments.append(
+            AudioSegment(
+                start=start,
+                end=end,
+                text=text,
+                source=source,
+                filename=f"{filename}_{len(segments)}",
+            )
+        )
+    audio = Audio(
+        filename=filename,
+        duration=sum([segment.duration for segment in segments]),
+        segments=segments,
+        source=source,
+    )
+    return audio
--- tests/services/test_vtt_split.py
+++ tests/services/test_vtt_split.py
@@ -6,21 +6,35 @@ from processing.config import Audio, AudioSegment, SourceEnum
 
 @pytest.fixture
 def vtt_file():
-    return (Path.cwd() / "test_data" / "test.ar.vtt").absolute()
+    return (Path("tests") / "test_data" / "test.vtt").absolute()
 
 
 def test_vtt_split_return_correct_segments(vtt_file):
-    audio = vtt_split(vtt_file, min_duration=8.0, max_duration=12.0, threshold=2.0, source=SourceEnum.MASC)
+    audio = vtt_split(
+        vtt_file,
+        min_duration=8.0,
+        max_duration=12.0,
+        threshold=2.0,
+        source=SourceEnum.MASC,
+    )
     expected_duration = [8.0, 11.0, 12.0, 11.0, 10.0, 8.0]
-    expected_text = ["1 2 3", "4 5", "6 7 8 9 10", "11 12 13 14", "15 16 17 18", "19 20"]
+    expected_text = [
+        "1 2 3",
+        "4 5",
+        "6 7 8 9 10",
+        "11 12 13 14",
+        "15 16 17 18",
+        "19 20",
+    ]
     assert isinstance(audio, Audio)
     assert audio.filename == "test"
     assert len(audio.segments) == 6
     assert audio.duration == sum(expected_duration)
-    for i, (segment, duration, text) in enumerate(zip(audio.segments, expected_duration, expected_text)):
+    for i, (segment, duration, text) in enumerate(
+        zip(audio.segments, expected_duration, expected_text)
+    ):
         assert isinstance(segment, AudioSegment)
         assert segment.duration == duration
         assert segment.source == SourceEnum.MASC
         assert segment.filename == f"test_{i}"
         assert segment.text == text
-    
