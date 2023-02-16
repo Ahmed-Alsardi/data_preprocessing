505dcc411cb0c97f252ea804f614ce8ef14fb3c0
Ahmed Alsardi <ahmed.alsardi.21@gmail.com>
2023-02-16 07:41:49 +0000

    split sada audios

--- src/processing/sada/sada_processing.py
+++ src/processing/sada/sada_processing.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import logging
 import pandas as pd
+from tqdm import tqdm
 from processing.config import SourceEnum
-from processing.services import clean_text
+from processing.services import clean_text, split_audio_to_segments
 
 
 logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
@@ -30,8 +31,9 @@ def read_sada_csv(csv_path: Path, do_clean_text: bool = True) -> pd.DataFrame:
 def create_segment_filename(
     df: pd.DataFrame, source: SourceEnum = SourceEnum.SADA
 ) -> pd.DataFrame:
-    df["FileName"] = df.FileName.apply(lambda x: Path(x).stem)
-    df["segment_filename"] = df.apply(lambda row: f"{row.FileName}_{row.name}", axis=1)
+    df["segment_filename"] = df.apply(
+        lambda row: f"{Path(row.FileName).stem}_{row.name}", axis=1
+    )
     df["source"] = source.value
     return df
 
@@ -58,6 +60,7 @@ def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
         ]
     ]
 
+
 def validate_datarame(df: pd.DataFrame) -> None:
     assert df.segment_text.notna().all()
     assert df.segment_duration.notna().all()
@@ -67,6 +70,7 @@ def validate_datarame(df: pd.DataFrame) -> None:
     assert df.audio_filename.notna().all()
     assert df.segment_duration.min() >= MIN_DURATION
     assert df.segment_duration.max() <= MAX_DURATION
+
 
 def process_sada_csv(
     csv_path: Path, source: SourceEnum = SourceEnum.SADA
@@ -79,14 +83,55 @@ def process_sada_csv(
     return df
 
 
+def split_audio_from_dataframe(
+    audio_path: Path,
+    audio_segments: pd.DataFrame,
+    output_path: Path,
+    extension: str = "wav",
+) -> None:
+    if not audio_path.exists():
+        raise ValueError(f"{audio_path} does not exist")
+    if not output_path.exists():
+        output_path.mkdir(parents=True, exist_ok=True)
+    if not output_path.is_dir():
+        raise ValueError(f"{output_path} is not a directory")
+    grouped = audio_segments.groupby("audio_filename")
+    total = 0
+    for audio_filename, group in tqdm(grouped, total=len(list(grouped))):
+        audio = audio_path / audio_filename
+        total += split_audio_to_segments(
+            audio=audio,
+            audio_segments=group,
+            output_dir=output_path,
+            extension=extension,
+        )
+    logger.info("total segments: %s, total of: %s", total, len(audio_segments))
+
+
 if __name__ == "__main__":
-    train_path = Path("/root/datasets/sada/train.csv")
-    dev_path = Path("/root/datasets/sada/dev.csv")
-    test_path = Path("/root/datasets/sada/test.csv")
+    root_path = Path("/root/datasets/sada")
     logger.info("process train data")
-    train_df = process_sada_csv(train_path)
+    train_df = process_sada_csv(root_path / "train.csv")
     logger.info("process dev data")
-    dev_df = process_sada_csv(dev_path)
+    dev_df = process_sada_csv(root_path / "dev.csv")
     logger.info("process test data")
-    test_df = process_sada_csv(test_path)
+    test_df = process_sada_csv(root_path / "test.csv")
     logger.info(f"train: {len(train_df)}, dev: {len(dev_df)}, test: {len(test_df)}")
+    logger.info("split train audios")
+    split_audio_from_dataframe(
+        audio_path=root_path,
+        audio_segments=train_df,
+        output_path=root_path / "train" / "segments",
+    )
+    logger.info("split dev audios")
+    split_audio_from_dataframe(
+        audio_path=root_path,
+        audio_segments=dev_df,
+        output_path=root_path / "dev" / "segments",
+    )
+    logger.info("split test audios")
+    split_audio_from_dataframe(
+        audio_path=root_path,
+        audio_segments=test_df,
+        output_path=root_path / "test" / "segments",
+    )
